ff5f72eb7de01aa483743c61f5e8fa807cf352c6
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2019-05-31 21:42:59 +0500

    proverka

--- simple-app/main.m
+++ simple-app/main.m
@@ -10,7 +10,6 @@
 
         //Задание 1
 
-
         // Создаем функцию определения соответствия введенной буквы английскому алфавиту
         void checkLetter(NSString* userLetter) {
             NSArray *letterBox = [[NSArray alloc] initWithObjects: @"a", @"b", @"c", @"d", @"e", @"f", @"g", @"h", @"i", @"j", @"k", @"l", @"m", @"n", @"o", @"p", @"q", @"r", @"s", @"t", @"u", @"v", @"w", @"x", @"w", @"z", nil];
